d61f468addf4e986eac7dfcf60bab02df5330894
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-02-20 06:43:43 -0300

    hacking it out

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var http = require('http');
+var fs = require('fs');
 var app = express();
 
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/public/index.html'));
+        console.log('gogo');
+        res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/isit', function (req, res) {
+        var url = req.query.url;
+        res.write('lets process ' + url);
+        res.end();
 });
 
 app.listen(80, function () {
   console.log('Listening on port 80');
+  console.log('what now?');
+  console.log(new Date());
 });
+
