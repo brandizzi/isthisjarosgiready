3b980e95d170c0ccb5f0ce61d91e53bfedbe44e0
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-02-21 21:43:20 -0300

    downloaded a file

--- index.js
+++ index.js
@@ -3,24 +3,54 @@ var morgan = require('morgan');
 var path = require('path');
 var http = require('http');
 var fs = require('fs');
+var request = require('request');
+var tmp = require('tmp');
+
+var showIndex = function showIndex(req, res) {
+        res.sendFile(path.join(__dirname + '/public/index.html'));
+};
+
+var checkJar = function (req, res) {
+        var url = req.query.url;
+        console.error('checking' + url);
+        downloadZipFileFromURL(url, function(err, contents) {
+        	console.log('here we are');
+        	console.log(err, contents);
+        	if (err) throw err;
+        	res.write(contents.toString());
+        	res.end();
+        });
+};
+
+var downloadToFile = function(url, path, cb) {
+	var stream = fs.createWriteStream(path);
+	console.log('downloading to file ' + path);
+
+	request(url).pipe(stream).on('close', function() {
+		fs.readFile(path, 'utf8', cb);
+	});
+};
+
+var downloadZipFileFromURL = function (url, cb) {
+        console.log('downloading zip '+ url);
+	tmp.file(function(err, path) {
+		console.log('create temp file ' + path);
+		if (err) throw err;
+		downloadToFile(url, path, cb);
+	});
+};
+
+
+
 var app = express();
 
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-        console.log('gogo');
-        res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+app.get('/', showIndex);
 
-app.get('/isit', function (req, res) {
-        var url = req.query.url;
-        res.write('lets process ' + url);
-        res.end();
-});
+app.get('/isit', checkJar);
 
 app.listen(80, function () {
   console.log('Listening on port 80');
-  console.log('what now?');
-  console.log(new Date());
 });
 
