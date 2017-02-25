c494c7f41576ca5361f0905d4fb8185c2db899a1
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-02-25 13:56:58 -0300

    a lot of refactoring

--- index.js
+++ index.js
@@ -12,77 +12,194 @@ var showIndex = function showIndex(req, res) {
         res.sendFile(path.join(__dirname + '/public/index.html'));
 };
 
+var errorReport = (err, failureType, statusCode, logMessage) => {
+    if (logMessage) {
+        console.log(logMessage);
+    }
+
+    return {
+        errorData: err,
+        failureType : failureType,
+        statusCode: statusCode
+    };
+}
+
+var fillErrorPage = (doc, url, report) => {
+    if (report.failureType == 'failure.cannot_parse_template') {
+        return '<p>Template error: ' + JSON.stringify(err);
+    }
+
+    var title = doc.getElementsByTagName("title");
+
+    title[0].textContent = "We could not check " + url + " :(";
+
+    var jarFile = doc.getElementsByClassName('jar-file');
+    jarFile[0].textContent = url;
+
+    var errorType = doc.getElementsByClassName('error-type');
+    errorType[0].textContent = report.failureType;
+
+    var errorData = doc.getElementsByClassName('error-data');
+    errorData[0].innerHTML = report.errorData;
+
+    return jsdom.serializeDocument(doc);
+};
+
+var respondError = (req, res, url, report) => {
+    jsdom.env({
+        file: "public/error.html",
+        done: (err, window) => {
+                console.log('report ', report);
+            if (err) {
+                report = errorReport(
+                    err, 'failure.cannot_parse_template', 500,
+                    'Failed to parse template public/error.html: ' + err);
+
+                res.status(report.statusCode);
+                res.write(fillErrorPage(doc, url, report));
+
+                res.end();
+                window.close();
+
+                return;
+            }
+
+            var doc = window.document;
+
+            res.status(report.statusCode);
+            res.write(fillErrorPage(doc, url, report));
+
+            res.end();
+            window.close();
+        }
+    });
+};
+
 var checkJar = function (req, res) {
-        var url = req.query.url;
-        console.error('checking' + url);
-        getManifestFileFromJarURL(url, function(err, contents) {
-        	console.log('here we are');
-        	console.log(err, contents);
-        	if (err) throw err;
+    var url = req.query.url;
 
-		var soughtKey = "Bundle-SymbolicName:"
-		contents = "\n" + contents;
-		var isOSGi = contents.includes(soughtKey);
+    getManifestFileFromJarURL(url, function(report, contents) {
+        if (report) {
+            respondError(req, res, url, report);
+            return; 
+        }
 
-        	jsdom.env({
-        		file: "public/jar-info.html",
-        		done: function(err, window) {
-        			var doc = window.document;
-        			var title = doc.getElementsByTagName("title");
-        			title[0].textContent = "Is " + url + " OSGI-ready?";
-        			var jarFile = doc.getElementsByClassName('jar-file');
-        			jarFile[0].textContent = url;
-        			var jarIsOSGi = doc.getElementsByClassName('jar-is-osgi');
-        			jarIsOSGi[0].innerHTML = ""+isOSGi;
-        			console.log(jarIsOSGi[0]);
-        			res.write(jsdom.serializeDocument(doc));
-        			res.end();
+        var soughtKey = "\nBundle-SymbolicName:"
+        contents = "\n" + contents;
+        var isOSGi = contents.includes(soughtKey);
 
-				window.close();
-        		}
-        	});
+        jsdom.env({
+            file: "public/jar-info.html",
+            FetchExternalResources: false,
+            ProcessExternalResources: false,
+            done: function(err, window) {
+                if (err) {
+                    var report = errorReport(
+                        err, 'failure.cannot_parse_template', 500,
+                        'Failed to parse template public/jar-info.html: ' + err);
+ 
+                    respondError(req, res, url, report);
+ 
+                    return;
+                }
 
+                var doc = window.document;
+                var title = doc.getElementsByTagName("title");
+
+                title[0].textContent = "Is " + url + " OSGI-ready?";
+
+                var jarFile = doc.getElementsByClassName('jar-file');
+
+                jarFile[0].textContent = url;
+
+                var jarIsOSGi = doc.getElementsByClassName('jar-is-osgi');
+
+                jarIsOSGi[0].innerHTML = ""+isOSGi;
+
+                res.write(jsdom.serializeDocument(doc));
+ 
+                res.end();
+                window.close();
+            }
         });
+
+    });
 };
 
-var downloadJarToFile = function(url, path, cb) {
-	var stream = fs.createWriteStream(path);
-	console.log('downloading to file ' + path);
+var downloadJarToFile = (url, path, cb) => {
+    var stream = fs.createWriteStream(path);
 
-	var req;
-	try {
-		req = request(url).pipe(stream);
-	} catch (err) {
-		cb(err);
-		return;
-	}
+    var req;
+    try {
+        console.log('downloading file '+ url);
+        req = request(url);
+        console.log('download completed');
+    } catch (err) {
+        var report = errorReport(
+            err, 'failure.cannot_get_file', 404,
+            'Failed to get file from ' + url + ': ' + err);
 
-	req.on('close', function(err) {
-		if (err) {
-			cb(err);
-			return;
-		}
+        cb(report);
 
-		var zip = new AdmZip(path);
+        return;
+    }
 
-		try {
-			var manifest = zip.readAsText('META-INF/MANIFEST.MF');
-		} catch (err) {
-			cb(err);
-			return;
-		}
+    var stream;
+    try {
+        console.log('streaming ' + url + ' content to file ' + path);
+        stream  = req.pipe(stream);
+        console.log('streaming completed.')
+    } catch (err) {
+        var report = errorReport(
+            err, 'failure.cannot_pipe_to_stream', 500,
+            'Failed to pipe ' + url + ' to ' + path ,+': ' + err);
 
-		cb(err, manifest);
-	});
+        cb(report);
+
+        return;
+    }
+
+    stream.on('finish', () => {
+        try {
+            console.log('opening zip file ' + path);
+            var zip = new AdmZip(path);
+            console.log('zip file opened.');
+            console.log('extracting MANIFEST.MF.');
+            var manifest = zip.readAsText('META-INF/MANIFEST.MF');
+            console.log('MANIFEST.MF extracted.');
+        } catch (err) {
+            console.log(err);
+            var report = errorReport(
+                err, 'failure.cannot_find_manifest_mf', 415,
+                'File at ' + url + 'is NO Zip or contains no MANIFEST.MF');
+
+            cb(report);
+
+            return;
+        }
+
+        cb(undefined, manifest);
+    });
 };
 
-var getManifestFileFromJarURL = function (url, cb) {
-        console.log('downloading zip '+ url);
-	tmp.file(function(err, path) {
-		console.log('create temp file ' + path);
-		if (err) throw err;
-		downloadJarToFile(url, path, cb);
-	});
+var getManifestFileFromJarURL = (url, cb) => {
+    console.log('downloading zip '+ url);
+
+    tmp.file((err, path) => {
+        console.log('create temp file ' + path);
+ 
+        if (err) {
+            var report = errorReport(
+                err, 'failure.cannot_create_temp_file', 500,
+                'Could not creawte a temporary file.');
+
+            cb(report);
+
+            return;
+        }
+
+        downloadJarToFile(url, path, cb);
+    });
 };
 
 
